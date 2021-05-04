d2bee250018adb737024d316b894f1824c9f3ac6
unknown <guerreiramaya2016@gmail.com>
2021-05-04 00:35:29 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/JennifferRufino" target="_blank" rel="noreferrer">Jenniffer Rufino</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/jenniffer_rufino" target="_blank" rel="noreferrer">@jenniffer_rufino</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
