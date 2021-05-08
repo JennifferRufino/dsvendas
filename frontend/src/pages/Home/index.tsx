f4779cc0e02a389c8bdbe84af8bf268af6a50165
unknown <guerreiramaya2016@gmail.com>
2021-05-08 16:39:42 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
+import {Link} from 'react-router-dom';
 
 function Home() {
     return (
-        <div>
-            <h2>Página Home</h2>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                </div>
+                <Link className = "btn btn-primary btn-lg" to="/dashboard"> Acessar Dashboard </Link>
+            </div>
+            <Footer />
+        </>
     );
 }
 
